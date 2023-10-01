9a12a7ac585ba20977078fcf5a40a6b38dd38bbe
DESKTOP-P12GRKK\Олександр <sacha.berdnuk@gmail.com>
2023-10-01 14:32:38 +0300

    add removal of extra spaces, conversion to lower case.

--- ExcelWebAPI/Controllers/ExcelController.cs
+++ ExcelWebAPI/Controllers/ExcelController.cs
@@ -2,6 +2,8 @@
 using ExcelWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Xml.Linq;
 
 namespace ExcelWebAPI.Controllers
 {
@@ -19,27 +21,50 @@ namespace ExcelWebAPI.Controllers
         [HttpGet("/{sheetId}")]
         public async Task<IActionResult> Get([FromRoute] string sheetId)
         {
+            sheetId = sheetId.Trim();
+
+            if (sheetId.Any(Char.IsWhiteSpace))
+            {
+                return BadRequest();
+            }
+
+            sheetId = sheetId.ToLower();
+
             Sheet? sheet = await _manager.GetSheetAsync(sheetId);
             if (sheet == null)
             {
                 return NotFound();
             }
+
             SheetDTO sheetDTO = new();
             foreach (var cell in sheet.Cells)
             {
                 sheetDTO.Cells.Add(cell.Id, new(cell.Value, await _manager.GetResult(sheetId, cell.Id, cell.Value)));
             }
+
             return Ok(sheetDTO);
         }
 
         [HttpGet("/{sheetId}/{sellId}")]
         public async Task<IActionResult> Get([FromRoute] string sheetId, [FromRoute] string sellId)
         {
+            sheetId = sheetId.Trim();
+            sellId = sellId.Trim();
+
+            if (sheetId.Any(Char.IsWhiteSpace) || sellId.Any(Char.IsWhiteSpace))
+            {
+                return BadRequest();
+            }
+
+            sheetId = sheetId.ToLower();
+            sellId = sellId.ToLower();
+
             Cell? cell = await _manager.GetSheetCellAsync(sheetId, sellId);
             if (cell == null) 
             {
                 return NotFound();
             }
+
             CellDTO cellDTO = new(cell.Value, await _manager.GetResult(sheetId, cell.Id, cell.Value));
             return Ok(cellDTO);
         }
@@ -47,12 +72,25 @@ namespace ExcelWebAPI.Controllers
         [HttpPost("/{sheetId}/{sellId}")]
         public async Task<IActionResult> Post([FromRoute] string sheetId, [FromRoute] string sellId, string value)
         {
+            sheetId = sheetId.Trim();
+            sellId = sellId.Trim();
+
+            if (sheetId.Any(Char.IsWhiteSpace) || sellId.Any(Char.IsWhiteSpace)) 
+            {
+                return BadRequest();
+            }
+
+            sheetId = sheetId.ToLower();
+            sellId =sellId.ToLower();
+
             Cell cell =  await _manager.SetSheetCellAsync(sheetId, sellId, value);
+           
             CellDTO cellDTO = new(cell.Value, await _manager.GetResult(sheetId, cell.Id, cell.Value));
             if(cellDTO.Value=="Error")
             {
                 return StatusCode(StatusCodes.Status422UnprocessableEntity, cellDTO);
             }
+
             return StatusCode(StatusCodes.Status201Created, cellDTO);
         }
     }

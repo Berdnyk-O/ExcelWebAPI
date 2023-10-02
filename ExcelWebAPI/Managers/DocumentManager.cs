f6a52d5d2c349d22ed644c82d83092f53c0fc862
DESKTOP-P12GRKK\Олександр <sacha.berdnuk@gmail.com>
2023-10-02 23:42:43 +0300

    add bracket handling

--- ExcelWebAPI/Managers/DocumentManager.cs
+++ ExcelWebAPI/Managers/DocumentManager.cs
@@ -39,7 +39,7 @@ namespace ExcelWebAPI.Managers
         {
             Sheet sheet = await GetSheetAsync(sheetId)
                            ?? await CreateSheetAsync(sheetId);
-                 
+
             Cell cell = await GetSheetCellAsync(sheet.Id, cellId)
                          ?? await CreateCellAsync(sheet.Id, cellId);
 
@@ -52,7 +52,7 @@ namespace ExcelWebAPI.Managers
 
         public async Task<Sheet?> GetSheetAsync(string sheetId)
         {
-            return await _context.Sheets.Include(x=>x.Cells).FirstOrDefaultAsync(x => x.Id == sheetId) ?? null;
+            return await _context.Sheets.Include(x => x.Cells).FirstOrDefaultAsync(x => x.Id == sheetId) ?? null;
         }
 
         public async Task<Cell?> GetSheetCellAsync(string sheetId, string cellId)
@@ -65,16 +65,47 @@ namespace ExcelWebAPI.Managers
             return await _context.Cells.FirstOrDefaultAsync(x => x.Id == cellId) ?? null;
         }
 
-        public async Task<string> GetResult(string sheetId,string cellId, string cellValue)
+        public async Task<string> GetResult(string sheetId, string cellId, string cellValue)
         {
-            if (cellValue[0]!='=')
+            if (cellValue[0] != '=')
             {
                 return cellValue;
             }
 
+            while (cellValue.Contains('(') && cellValue.Contains(')'))
+            {
+                var index1 = cellValue.LastIndexOf('(');
+                int index2 = 0;
+                for (int i = index1+1; i < cellValue.Length; i++)
+                {
+                    char currentChar = cellValue[i];
+                    if (currentChar == ')')
+                    {
+                        index2 = i;
+                        break;
+                    }
+                }
+                if(index2==0)
+                {
+                    return "ERROR";
+                }
+                string substring = '=' + cellValue[(index1 + 1)..index2];
+                string result = await GetResult(sheetId, cellId, substring);
+
+                substring = cellValue[index1..(index2 + 1)];
+                cellValue = cellValue.Replace(substring, result);
+            }
+
+            if (cellValue.Contains('(') && !cellValue.Contains(')')
+                || !cellValue.Contains('(') && cellValue.Contains(')'))
+            {
+                return "ERROR";
+            }
+
+
             List<string> operators = new();
-            SplitIntoOperators(cellValue,ref operators);
-            if(operators.Contains(cellId))
+            SplitIntoOperators(cellValue, ref operators);
+            if (operators.Contains(cellId))
             {
                 return "ERROR";
             }
@@ -91,11 +122,11 @@ namespace ExcelWebAPI.Managers
             }
 
             List<char> operations = new();
-            SplitIntoOperations(cellValue,ref operations);
+            SplitIntoOperations(cellValue, ref operations);
 
-            string res="";
-            char op=' ';
-            while (operations.Count>0)
+            string res = "";
+            char op = ' ';
+            while (operations.Count > 0)
             {
                 int index = 0;
                 if (operations.Contains('*'))
@@ -125,12 +156,12 @@ namespace ExcelWebAPI.Managers
             }
             return res;
         }
-        private void SplitIntoOperators(string cellValue,ref List<string> operators)
+        private void SplitIntoOperators(string cellValue, ref List<string> operators)
         {
             operators = cellValue.Split('=', '+', '-', '*', '/').ToList();
             operators = operators.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
         }
-        private void SplitIntoOperations(string cellValue,ref List<char> operations)
+        private void SplitIntoOperations(string cellValue, ref List<char> operations)
         {
             char[] operators = { '+', '-', '*', '/' };
 
@@ -189,9 +220,26 @@ namespace ExcelWebAPI.Managers
                         return (intFirstValue / intSecondValue).ToString();
                     default:
                         return "Error";
-                } 
+                }
             }
             return "Error";
+        }
+        private void GetIddexesOfBreckets(List<int> openBrackets, List<int> closeBrackets, string cellValue)
+        {
+            for (int i = 0; i < cellValue.Length; i++)
+            {
+                char currentChar = cellValue[i];
+
+                if (currentChar == '(')
+                {
+                    openBrackets.Add(i);
+                }
+                else if (currentChar == ')')
+                {
+                    closeBrackets.Add(i);
+                }
+            }
+
         }
     }
 }
